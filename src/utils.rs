5372d13f75d13403d2b6bcd045926e0019dacd11
yaobo <263384374@qq.com>
2025-09-29 13:38:44 +0800

    fix():

--- src/utils.rs
+++ src/utils.rs
@@ -1,10 +1,19 @@
 #![allow(dead_code)]
 use crate::errors::HWIDError;
 use anyhow::Error;
+use md5::{Digest, Md5};
 use netdev::{get_interfaces, Interface};
 use std::fs::OpenOptions;
 use std::io::{Read, Write};
 use uuid::Uuid;
+
+pub fn get_md5(txt: String) -> String {
+    let data = txt.as_bytes();
+    let mut hasher = Md5::new();
+    hasher.update(data);
+    let result = hasher.finalize();
+    format!("{:x}", result)
+}
 
 pub(crate) fn file_token(path: &str) -> Result<String, HWIDError> {
     let mut file = OpenOptions::new()
@@ -54,4 +63,10 @@ pub(crate) fn get_mac_addr() -> Result<String, Error> {
         return Err(anyhow::Error::msg("No valid MAC address found"));
     }
     Ok(macs.join("|"))
+}
+
+#[test]
+fn test_md5() {
+    let str = "hello 12121212world dad ik223j2j22jj22222222222222222222sdasdfsdf".to_string();
+    println!("{}", get_md5(str));
 }
